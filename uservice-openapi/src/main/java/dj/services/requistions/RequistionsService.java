81c5485699e7e0d6f0e3482af488678a94252199
DamianJarzebowski <damian.r.hajduk@o2.pl>
2023-03-04 22:57:57 +0100

    wip

--- uservice-openapi/src/main/java/dj/services/requistions/RequistionsService.java
+++ uservice-openapi/src/main/java/dj/services/requistions/RequistionsService.java
@@ -1,16 +1,10 @@
-package dj.services.requistions;
+    package dj.services.requistions;
 
 import java.net.URI;
 
 public interface RequistionsService {
 
-    ResponseEntity<?> createConnection(String institutionId);
-
-    String getListAccounts(String reference);
-    
-
     URI createConnection(String institutionId);
 
     String getListAccounts(String reference);
->>>>>>> 979c102c51f40abd2d6fff37b3cced89c9095325
 }
