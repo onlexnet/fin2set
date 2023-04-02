6fe7a8e90ff2a0029ba6436233a3b1dd4975c4d6
Slawomir Siudek <slawomir@siudek.net>
2023-04-02 17:18:02 +0200

    Enable CORS

--- uservice-openapi/README.md
+++ uservice-openapi/README.md
@@ -13,7 +13,7 @@ http://localhost:8080/swagger-ui/#
 az acr login --name pocfin2set
 mvnd clean install -DskipTests # to build binaries
 mvnd compile jib:dockerBuild # to push image to local repository - should push to remote, but I don't know why it is in local
-docker push pocfin2set.azurecr.io/fin2set:latest # push image to remove registry
+docker push pocfin2set.azurecr.io/fin2set:latest # push just created image to remove registry
 ```
 
 ## Useful links:
--- uservice-openapi/src/main/java/dj/api/SecurityConfiguration.java
+++ uservice-openapi/src/main/java/dj/api/SecurityConfiguration.java
@@ -5,17 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
-    
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .cors()
-                .and()
-                .authorizeHttpRequests(it -> it.anyRequest().permitAll())
-                .build();
-    }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http
+        .cors(cors -> cors
+            .configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
+        .authorizeHttpRequests(it -> it.anyRequest().permitAll())
+        .build();
+  }
 }
