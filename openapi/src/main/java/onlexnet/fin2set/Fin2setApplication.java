414137ff94341bb5de4454bea86fd524c0754984
Slawomir Siudek <slawomir@siudek.net>
2023-04-04 23:18:13 +0200

    Initial commit

---
+++ openapi/src/main/java/onlexnet/fin2set/Fin2setApplication.java
@@ -0,0 +1,13 @@
+package onlexnet.fin2set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fin2setApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fin2setApplication.class, args);
+	}
+
+}
