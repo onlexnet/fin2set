8781899fe7d050896d8ce1ceb1e7df17246f6569
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-04-18 16:08:46 +0000

    WIP

--- uservice-openapi/src/main/java/onlexnet/fin2set/domain/models/BankUserDetailsConnection.java
+++ uservice-openapi/src/main/java/onlexnet/fin2set/domain/models/BankUserDetailsConnection.java
@@ -5,8 +5,6 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -14,41 +12,30 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class BankUserDetailsConnection {
 
-    @JsonProperty("id")
     private UUID id;
 
-    @JsonProperty("status")
     private Object status;
 
-    @JsonProperty("Bank_id")
     private String bankId;
 
-    @JsonProperty("accounts")
     private List<UUID> accounts;
 
-    @JsonProperty("link")
     private URI link;
 
-    @JsonProperty("end_user_agreement_info")
     private EndUserAgreementInfo endUserAgreementInfo;
 
     @Data
     @Accessors(chain = true)
     public static class EndUserAgreementInfo {
 
-        @JsonProperty("created")
         private OffsetDateTime created;
 
-        @JsonProperty("max_historical_days")
         private Integer maxHistoricalDays;
 
-        @JsonProperty("access_valid_for_days")
         private Integer accessValidForDays;
 
-        @JsonProperty("access_scope")
         private List<String> accessScope;
 
-        @JsonProperty("accepted")
         private OffsetDateTime accepted;
 
     }
