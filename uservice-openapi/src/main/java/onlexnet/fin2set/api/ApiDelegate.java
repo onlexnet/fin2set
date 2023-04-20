920a37b385505c40688abd1c64f3a785cc774dab
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-04-20 18:17:24 +0000

    create schema api and implement generated api

---
+++ uservice-openapi/src/main/java/onlexnet/fin2set/api/ApiDelegate.java
@@ -0,0 +1,60 @@
+package onlexnet.fin2set.api;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import onlexnet.fin2set.domain.models.Bank;
+import onlexnet.fin2set.domain.models.BankStatement;
+import onlexnet.fin2set.domain.models.BankUserDetailsConnection;
+import onlexnet.fin2set.generated.api.ApiApiDelegate;
+import onlexnet.fin2set.host.AuthProvider;
+import onlexnet.fin2set.nordigen.NordigenFacade;
+
+@Component
+@RequiredArgsConstructor
+final class ApiDelegate implements ApiApiDelegate {
+
+  private final AuthProvider ap;
+  private final NordigenFacade nordigenFacade;
+
+  @Override
+  public ResponseEntity<String> whoAmI() {
+    return ResponseEntity.ok(ap.current().toString());
+  }
+
+  @Override
+  public ResponseEntity<List<Bank>> getListBanks(String country) {
+    return ResponseEntity.ok(nordigenFacade.getListBanks(country));
+  }
+
+  @Override
+  public ResponseEntity<Bank> getBank(String bankID) {
+    return ResponseEntity.ok(nordigenFacade.getBank(bankID));
+  }
+
+  @Override
+  public ResponseEntity<URI> generateUriToConnectionWithBankAccount(String bankID) {
+    return ResponseEntity.ok(nordigenFacade.createLinkToConnect(bankID));
+  }
+
+  @Override
+  public ResponseEntity<BankUserDetailsConnection> catchUser(String reference) {
+    return ResponseEntity.ok(nordigenFacade.catchUserAndGetInfoAboutConection(reference));
+  }
+
+  @Override
+  public ResponseEntity<BankUserDetailsConnection> getInfoAboutUserConnection(String connectionID) {
+    return ResponseEntity.ok(nordigenFacade.catchUserAndGetInfoAboutConection(connectionID));
+  }
+
+  @Override
+  public ResponseEntity<BankStatement> getBankstatement(UUID accountID) {
+    return ResponseEntity.ok(nordigenFacade.getBankStatement(accountID));
+  }
+
+}
