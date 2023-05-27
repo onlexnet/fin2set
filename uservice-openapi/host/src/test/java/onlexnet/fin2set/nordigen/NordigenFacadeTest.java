d00e7386abda62ab00ccb364194514df053b7477
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-05-27 14:42:47 +0000

    Automatization playwright test

--- uservice-openapi/host/src/test/java/onlexnet/fin2set/nordigen/NordigenFacadeTest.java
+++ uservice-openapi/host/src/test/java/onlexnet/fin2set/nordigen/NordigenFacadeTest.java
@@ -2,17 +2,23 @@ package onlexnet.fin2set.nordigen;
 
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.LoadState;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import onlexnet.fin2set.db.DbExtension;
 
 @SpringBootTest
@@ -23,42 +29,54 @@ public class NordigenFacadeTest {
   @Autowired
   NordigenFacade nordigen;
 
+  static Playwright playwright;
+  static Browser browser;
+
+  static BrowserContext context;
+  static Page page;
+
+  @BeforeAll
+  static void launchBrowser() {
+    playwright = Playwright.create();
+    browser = playwright.chromium().launch();
+  }
+
+  @BeforeEach
+  static void createContextAndPage() {
+    context = browser.newContext();
+    page = context.newPage();
+  }
+
+  @AfterAll
+  static void closeBrowser() {
+    browser.close();
+    playwright.close();
+  }
+
+  @AfterEach
+  static void closeContext() {
+    context.close();
+  }
+
+  // Host system is missing dependencies to run browsers. 
+  // Read logs to install missing dependencies.
   @Test
   void loadData() {
     var link = nordigen.createLinkToConnect("SANDBOXFINANCE_SFIN0000");
-    try (Playwright playwright = Playwright.create()) {
 
-      // Host system is missing dependencies to run browsers. ║
-      // ║ Please install them with the following command: ║
-      // ║ ║
-      // ║ sudo mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D
-      // exec.args="install-deps" ║
-      // ║ ║
-      // ║ Alternatively, use apt: ║
-      // ║ sudo apt-get install libgbm1 ║
-      // ║ ║
-      // ║ [3 Playwright Team
-      
-      var browserType = playwright.chromium();
-      var browserSettings = new BrowserType.LaunchOptions().setHeadless(true).setSlowMo(2000);
-      var browser = browserType.launch(browserSettings);
-      var page = browser.newPage();
+    page.navigate(link.toString());
+    page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-1 Agreement and information for user.png")));
+    page.click("INPUT[type='submit']");
 
-      page.navigate(link.toString());
-      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-1 Agreement and information for user.png")));
-      page.click("INPUT[type='submit']");
-      
-      page.waitForSelector("INPUT[type='submit']");
-      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-2 Bank login site.png")));
-      page.click("INPUT[type='submit']");
+    page.waitForSelector("INPUT[type='submit']");
+    page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-2 Bank login site.png")));
+    page.click("INPUT[type='submit']");
 
-      page.waitForSelector("text=approve");
-      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-3 Accept agreement on bank site.png")));
-      page.click("text=approve");
+    page.waitForSelector("text=approve");
+    page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-3 Accept agreement on bank site.png")));
+    page.click("text=approve");
 
-      page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-4 Our page.png")));
-
-    }
+    page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-4 Our page.png")));
 
   }
 }
