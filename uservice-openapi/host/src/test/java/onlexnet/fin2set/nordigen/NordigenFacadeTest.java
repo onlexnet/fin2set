0021ac6197daaa05537f28822dbf9189519d0ef8
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-05-27 17:51:46 +0000

    fix NordigenFacadeTest

--- uservice-openapi/host/src/test/java/onlexnet/fin2set/nordigen/NordigenFacadeTest.java
+++ uservice-openapi/host/src/test/java/onlexnet/fin2set/nordigen/NordigenFacadeTest.java
@@ -2,61 +2,25 @@ package onlexnet.fin2set.nordigen;
 
 import java.nio.file.Paths;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.ElementHandle.ClickOptions;
+import com.microsoft.playwright.options.WaitUntilState;
 
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
 import onlexnet.fin2set.db.DbExtension;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @ExtendWith(DbExtension.class)
-public class NordigenFacadeTest {
+public class NordigenFacadeTest extends ScriptBase {
 
   @Autowired
   NordigenFacade nordigen;
-
-  static Playwright playwright;
-  static Browser browser;
-
-  static BrowserContext context;
-  static Page page;
-
-  @BeforeAll
-  static void launchBrowser() {
-    playwright = Playwright.create();
-    browser = playwright.chromium().launch();
-  }
-
-  @BeforeEach
-  static void createContextAndPage() {
-    context = browser.newContext();
-    page = context.newPage();
-  }
-
-  @AfterAll
-  static void closeBrowser() {
-    browser.close();
-    playwright.close();
-  }
-
-  @AfterEach
-  static void closeContext() {
-    context.close();
-  }
 
   // Host system is missing dependencies to run browsers. 
   // Read logs to install missing dependencies.
@@ -64,7 +28,9 @@ public class NordigenFacadeTest {
   void loadData() {
     var link = nordigen.createLinkToConnect("SANDBOXFINANCE_SFIN0000");
 
-    page.navigate(link.toString());
+    page.navigate(link.toString(), new Page.NavigateOptions()
+    .setTimeout(5000)
+    .setWaitUntil(WaitUntilState.NETWORKIDLE));
     page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-1 Agreement and information for user.png")));
     page.click("INPUT[type='submit']");
 
@@ -77,6 +43,5 @@ public class NordigenFacadeTest {
     page.click("text=approve");
 
     page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("Step-4 Our page.png")));
-
   }
 }
