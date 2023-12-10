d583afb904bbd81ba6e2d223a05e3cdc23c02bad
siudeks <slawomir@siudek.net>
2023-12-10 21:01:00 +0000

    More chaotic tests

--- uservice-webapi/host/src/main/java/onlexnet/webapi/openai/OpenAi.java
+++ uservice-webapi/host/src/main/java/onlexnet/webapi/openai/OpenAi.java
@@ -1,16 +1,34 @@
 package onlexnet.webapi.openai;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import com.azure.ai.openai.OpenAIClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.ai.openai.OpenAIServiceVersion;
+import com.azure.ai.openai.models.ChatChoice;
+import com.azure.ai.openai.models.ChatCompletions;
 import com.azure.ai.openai.models.ChatCompletionsOptions;
 import com.azure.ai.openai.models.ChatMessage;
 import com.azure.ai.openai.models.ChatRole;
+import com.azure.ai.openai.models.CompletionsFinishReason;
+import com.azure.ai.openai.models.EmbeddingItem;
+import com.azure.ai.openai.models.Embeddings;
+import com.azure.ai.openai.models.EmbeddingsOptions;
+import com.azure.ai.openai.models.EmbeddingsUsage;
+import com.azure.ai.openai.models.FunctionCall;
+import com.azure.ai.openai.models.FunctionCallConfig;
+import com.azure.ai.openai.models.FunctionDefinition;
 import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +39,8 @@ import onlexnet.webapi.config.Secrets;
 public class OpenAi {
 
   private final Secrets secrets;
-  private final String deploymentName = "gpt-4-plugins";
+  private final String chatModel = "gpt-4-plugins";
+  private final String embeddingsModel = "text-embedding-ada-002";
 
   OpenAIClient client;
 
@@ -31,6 +50,7 @@ public class OpenAi {
         .credential(new AzureKeyCredential(secrets.openaiKey))
         .endpoint(secrets.openaiEndpoint)
         .buildClient();
+
   }
 
   public String getContinuation(List<Message> messages) {
@@ -39,12 +59,148 @@ public class OpenAi {
           var dtoRole = it.role() == MessageRole.ASSISTANT ? ChatRole.ASSISTANT : ChatRole.USER;
           return new ChatMessage(dtoRole, it.text());
         })
-        .toList();
-    var options = new ChatCompletionsOptions(dtoMessages);
+        .collect(Collectors.toList());
 
-    var cc = client.getChatCompletions(deploymentName, options);
-    var choice = cc.getChoices().get(0);
-    return choice.getMessage().getContent();
+    var functions = Arrays.asList(
+        new FunctionDefinition("getCurrentWeather")
+            .setDescription("Get the current weather")
+            .setParameters(BinaryData.fromObject(getFunctionDefinition())));
+
+    var options = new ChatCompletionsOptions(dtoMessages)
+        .setFunctionCall(FunctionCallConfig.AUTO)
+        .setFunctions(functions);
+
+    var chatCompletions = client.getChatCompletions(chatModel, options);
+
+    var chatMessages2 = handleFunctionCallResponse(chatCompletions.getChoices(), dtoMessages);
+
+    // Take your function_call result as the input prompt to make another request to
+    // service.
+    ChatCompletionsOptions chatCompletionOptions2 = new ChatCompletionsOptions(chatMessages2);
+    ChatCompletions chatCompletions2 = client.getChatCompletions(chatModel, chatCompletionOptions2);
+    List<ChatChoice> choices = chatCompletions2.getChoices();
+    var message = choices.get(0).getMessage();
+    var result = message.getContent();
+    return result;
   }
 
+  // hint:
+  // https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/usage/GetEmbeddingsSample.java
+  public List<Double> getEmbedings(String text) {
+    var embeddingsOptions = new EmbeddingsOptions(Arrays.asList(text));
+    Embeddings embeddings = client.getEmbeddings(embeddingsModel, embeddingsOptions);
+
+    EmbeddingItem item = embeddings.getData().get(0);
+    // System.out.printf("Index: %d.%n", item.getPromptIndex());
+    var embedding = item.getEmbedding();
+
+    EmbeddingsUsage usage = embeddings.getUsage();
+    System.out.printf(
+        "Usage: number of prompt token is %d and number of total tokens in request and response is %d.%n",
+        usage.getPromptTokens(), usage.getTotalTokens());
+    return embedding;
+  }
+
+  private static Map<String, Object> getFunctionDefinition() {
+    // Construct JSON in Map, or you can use create your own customized model.
+    Map<String, Object> location = new HashMap<>();
+    location.put("type", "string");
+    location.put("description", "The city and state, e.g. San Francisco, CA");
+    Map<String, Object> unit = new HashMap<>();
+    unit.put("type", "string");
+    unit.put("enum", Arrays.asList("celsius", "fahrenheit"));
+    Map<String, Object> prop1 = new HashMap<>();
+    prop1.put("location", location);
+    prop1.put("unit", unit);
+    Map<String, Object> functionDefinition = new HashMap<>();
+    functionDefinition.put("type", "object");
+    functionDefinition.put("required", Arrays.asList("location", "unit"));
+    functionDefinition.put("properties", prop1);
+    return functionDefinition;
+  }
+
+  private static List<ChatMessage> handleFunctionCallResponse(List<ChatChoice> choices,
+      List<ChatMessage> chatMessages) {
+    for (ChatChoice choice : choices) {
+      ChatMessage choiceMessage = choice.getMessage();
+      FunctionCall functionCall = choiceMessage.getFunctionCall();
+      // We are looking for finish_reason = "function call".
+      if (CompletionsFinishReason.FUNCTION_CALL.equals(choice.getFinishReason())) {
+        // We call getCurrentWeather() and pass the result to the service.
+        System.out.printf("Function name: %s, arguments: %s.%n", functionCall.getName(), functionCall.getArguments());
+        // WeatherLocation is our class that represents the parameters to use in our
+        // function call.
+        // We deserialize and pass it to our function.
+        WeatherLocation weatherLocation = BinaryData.fromString(functionCall.getArguments())
+            .toObject(WeatherLocation.class);
+
+        int currentWeather = getCurrentWeather(weatherLocation);
+        chatMessages.add(new ChatMessage(ChatRole.USER, String.format("The weather in %s is %d degrees %s.",
+            weatherLocation.getLocation(), currentWeather, weatherLocation.getUnit())));
+      } else {
+        var messageHistory = new ChatMessage(ChatRole.ASSISTANT, choiceMessage.getContent());
+        messageHistory.setFunctionCall(choiceMessage.getFunctionCall());
+        chatMessages.add(messageHistory);
+      }
+    }
+    return chatMessages;
+  }
+
+  // This is the method we offer to OpenAI to be used as a function_call.
+  // For this example, we ignore the input parameter and return a simple value.
+  private static int getCurrentWeather(WeatherLocation weatherLocation) {
+    return 35;
+  }
+
+  // WeatherLocation is used for this sample. This describes the parameter of the
+  // function you want to use.
+  private static class WeatherLocation {
+    @JsonProperty(value = "unit")
+    String unit;
+    @JsonProperty(value = "location")
+    String location;
+
+    @JsonCreator
+    WeatherLocation(@JsonProperty(value = "unit") String unit, @JsonProperty(value = "location") String location) {
+      this.unit = unit;
+      this.location = location;
+    }
+
+    public String getUnit() {
+      return unit;
+    }
+
+    public String getLocation() {
+      return location;
+    }
+  }
+
+  // examples
+  // https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/openai/azure-ai-openai/src/samples/java/com/azure/ai/openai/FunctionCallSample.java
+  // https://learn.microsoft.com/en-us/java/api/com.azure.ai.openai.models.functiondefinition?view=azure-java-preview
+  // :https://cobusgreyling.medium.com/practical-examples-of-openai-function-calling-a6419dc38775
+  // "name": "order_detail",
+  // "description": "template to capture an order.",
+  // "parameters": {
+  // "type": "object",
+  // "properties": {
+  // "to_address": {
+  // "type": "string",
+  // "description": "To address for the delivery"
+  // },
+  // "order": {
+  // "type": "string",
+  // "description": "The detail of the order"
+  // },
+  // "date": {
+  // "type": "string",
+  // "description": "the date for delivery."
+  // },
+  // "notes": {
+  // "type": "string",
+  // "description": "Any delivery notes."
+  // }
+  // }
+  // }
+  // }
 }
--- uservice-webapi/host/src/test/java/onlexnet/webapi/openai/OpenAiTest.java
+++ uservice-webapi/host/src/test/java/onlexnet/webapi/openai/OpenAiTest.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import onlexnet.webapi.DaprExtension;
-import onlexnet.webapi.openai.Message;
-import onlexnet.webapi.openai.MessageRole;
-import onlexnet.webapi.openai.OpenAi;
 
 @SpringBootTest
 @ExtendWith(DaprExtension.class)
@@ -29,4 +26,38 @@ public class OpenAiTest {
 
     Assertions.assertThat(response).isEqualTo("Hello");
   }
+
+  @Test
+  void shouldUseApi() {
+    var query = "What should I wear in Boston depending on the weather? Please advice with no more that 10 words.";
+    var initial = new Message(query, MessageRole.USER);
+
+    var response = openAi.getContinuation(List.of(initial));
+
+    var a = openAi.getEmbedings(response);
+    var b = openAi.getEmbedings("Light clothing, sunglasses, and sunscreen.");
+
+    var similarity = cosineSimilarity(a, b);
+    Assertions.assertThat(similarity).isGreaterThan(0.9);
+  }
+
+
+  // https://stackoverflow.com/questions/520241/how-do-i-calculate-the-cosine-similarity-of-two-vectors
+  public static double cosineSimilarity(List<Double> vectorA, List<Double> vectorB) {
+    return cosineSimilarity(vectorA.stream().mapToDouble(it -> it).toArray(), vectorB.stream().mapToDouble(it -> it).toArray());
+  }
+
+  public static double cosineSimilarity(double[] vectorA, double[] vectorB) {
+    double dotProduct = 0.0;
+    double normA = 0.0;
+    double normB = 0.0;
+    for (int i = 0; i < vectorA.length; i++) {
+        dotProduct += vectorA[i] * vectorB[i];
+        normA += Math.pow(vectorA[i], 2);
+        normB += Math.pow(vectorB[i], 2);
+    }   
+    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+}
+
+
 }
