5ab3e87b2f32879dffaa89823d17fe76ad384054
siudeks <slawomir@siudek.net>
2023-12-11 09:30:06 +0000

    Introduce BDD tests

--- uservice-webapi/README.md
+++ uservice-webapi/README.md
@@ -17,3 +17,6 @@ dapr run --resources-path=components --dapr-http-port 3601
 curl http://localhost:3601/v1.0/secrets/azurekeyvault/OPENAI-KEY
 ```
 
+## Interesting links
+- https://docs.spring.io/spring-ai/reference/concepts.html
+- https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/openai/azure-ai-openai/README.md
---
+++ uservice-webapi/host/src/test/java/onlexnet/webapi/features/ExampleSteps.java
@@ -0,0 +1,19 @@
+package onlexnet.webapi.features;
+
+import io.cucumber.java.en.*;
+
+public class ExampleSteps {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
