f0531b7cff6fbd2e089f4e81370c01168b4a7e65
siudeks <slawomir@siudek.net>
2024-01-22 19:37:16 +0000

    Openapi module

--- uservice-webapi/host/src/main/java/onlexnet/webapi/infra/AppEnvironment.java
+++ uservice-webapi/host/src/main/java/onlexnet/webapi/infra/AppEnvironment.java
@@ -22,11 +22,15 @@ import lombok.SneakyThrows;
 
 /**
  * For proper working the application needs some extra environment variables.
- * Because it need to be defined before creation of beans, it is done using {@link EnvironmentPostProcessor}.
+ * Because it need to be defined before creation of beans, it is done using
+ * {@link EnvironmentPostProcessor}.
  * We need to add two main sources:
- * - DAPR secrets. Of course app may read DAPR secrets using imperative code, but we would like to have them injected as it is best practice.
- *   This part may be removed in the future if/when Spring have extension to get such secrets from DAP by some kind of binding. 
- * - .env file, reused only for local development and tests. The file is not used / does not exist on prod
+ * - DAPR secrets. Of course app may read DAPR secrets using imperative code,
+ * but we would like to have them injected as it is best practice.
+ * This part may be removed in the future if/when Spring have extension to get
+ * such secrets from DAP by some kind of binding.
+ * - .env file, reused only for local development and tests. The file is not
+ * used / does not exist on prod
  */
 @Order(Ordered.LOWEST_PRECEDENCE)
 class AppEnvironment implements EnvironmentPostProcessor {
@@ -36,13 +40,16 @@ class AppEnvironment implements EnvironmentPostProcessor {
 
     var activeProfiles = Set.of(env.getActiveProfiles());
 
-    if (activeProfiles.contains("test")) {
-      loadEnvValues(env, "../");
-    }
-
-    if (activeProfiles.contains("local")) {
-      loadEnvValues(env, "./");
-    }
+    var profileAndEnvLocation = Map.of(
+        "test", "../",
+        "local", "./");
+    profileAndEnvLocation.entrySet()
+        .stream()
+        .filter(it -> activeProfiles.contains(it.getKey()))
+        .forEach(kv -> {
+          var value = kv.getValue();
+          loadEnvValues(env, value);
+        });
 
     loadDaprSecrets(env);
 
@@ -55,37 +62,35 @@ class AppEnvironment implements EnvironmentPostProcessor {
   private void loadDaprSecrets(ConfigurableEnvironment env) {
     var propertySources = env.getPropertySources();
 
-
     var daprConfig = new HashMap<String, Object>();
     registerAsSystemProperty(Properties.GRPC_PORT, env, daprConfig);
     registerAsSystemProperty(Properties.HTTP_PORT, env, daprConfig);
     var daprConfigPropertySource = new MapPropertySource("dapr-config-" + SECRET_STORE_NAME, daprConfig);
     propertySources.addLast(daprConfigPropertySource);
 
-
     var maxTimeWaitingForDaprInMilis = 10_000;
-    @Cleanup
-    var client = new DaprClientBuilder()
-        .build();
-    client.waitForSidecar(maxTimeWaitingForDaprInMilis).block();
-      
-    // load all secrets from azure keyvault and inject them as environment variables
-    var secretsAsStrangeMap = client.getBulkSecret(SECRET_STORE_NAME, Map.of()).block();
-    var secrets = secretsAsStrangeMap.entrySet().stream()
-        .map(it -> it.getValue())
-        .flatMap(it -> it.entrySet().stream())
-        .collect(Collectors.toMap(it -> it.getKey(), it -> (Object) it.getValue(), (v1, v2) -> v1));
-    var propertySource = new MapPropertySource("dapr-" + SECRET_STORE_NAME, secrets);
-    propertySources.addLast(propertySource);
+    try (var client = new DaprClientBuilder().build()) {
+      client.waitForSidecar(maxTimeWaitingForDaprInMilis).block();
+
+      // load all secrets from azure keyvault and inject them as environment variables
+      var secretsAsStrangeMap = client.getBulkSecret(SECRET_STORE_NAME, Map.of()).block();
+      var secrets = secretsAsStrangeMap.entrySet().stream()
+          .map(it -> it.getValue())
+          .flatMap(it -> it.entrySet().stream())
+          .collect(Collectors.toMap(it -> it.getKey(), it -> (Object) it.getValue(), (v1, v2) -> v1));
+      var propertySource = new MapPropertySource("dapr-" + SECRET_STORE_NAME, secrets);
+      propertySources.addLast(propertySource);
+      }
   }
 
   /**
-   * @param envFilePath folder where we may find .env file with optional environment variables
+   * @param envFilePath folder where we may find .env file with optional
+   *                    environment variables
    */
   void loadEnvValues(ConfigurableEnvironment env, String envPath) {
     var dotenv = Dotenv.configure()
-      .directory(envPath)
-      .load();
+        .directory(envPath)
+        .load();
 
     var envMap = new HashMap<String, Object>();
     dotenv.entries().forEach(it -> {
@@ -97,12 +102,16 @@ class AppEnvironment implements EnvironmentPostProcessor {
     propertySources.addLast(propertySource);
   }
 
-  private void registerAsSystemProperty(Property<?> prop, ConfigurableEnvironment readFrom, HashMap<String, Object> systemProperties) {
+  private void registerAsSystemProperty(Property<?> prop, ConfigurableEnvironment readFrom,
+      HashMap<String, Object> systemProperties) {
     var propEnvName = prop.getEnvName();
     var propName = prop.getName();
     var envValue = readFrom.getProperty(propEnvName);
-    if (envValue == null) return;
-    systemProperties.put(propName, envValue);
-    System.setProperty(propName, envValue);
+    Optional
+        .ofNullable(envValue)
+        .ifPresent(it -> {
+          systemProperties.put(propName, it);
+          System.setProperty(propName, it);
+        });
   }
 }
