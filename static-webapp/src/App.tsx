9f41e07a268e5e3aa3a5fa751f79a3f929d3c87b
siudeks <slawomir@siudek.net>
2024-01-23 23:02:08 +0000

    Test auth0

--- static-webapp/src/App.tsx
+++ static-webapp/src/App.tsx
@@ -3,22 +3,49 @@ import './App.css';
 import { useMyqueryQuery } from './api/gql/graphql';
 import { MainView } from './views/main/Main.View';
 
-function App() {
-  const { loading, error } = useMyqueryQuery({
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect } from 'react';
+
+
+export const App: React.FC<{}> = props => {
+  
+  const { loading, error: queryError } = useMyqueryQuery({
   });
+  const { loginWithRedirect, isAuthenticated, isLoading, error } = useAuth0();
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (isAuthenticated) return;
+
+    loginWithRedirect()
+  }, [loginWithRedirect, isAuthenticated, isLoading]);
+  
+  if (error) {
+    alert('Error:' + JSON.stringify(error));
+    return (<Label>Loading ....</Label>);
+  }
 
   if (loading) return (<Label>Loading ....</Label>);
 
-  if (error) return (<Label>Error!</Label>);
+  if (queryError) return (<Label>Error!</Label>);
 
-  const view = <FluentProvider theme={webLightTheme}>
-    <div className="App">
-      <MainView />
-    </div>
-  </FluentProvider>;
+  alert(`isLoading: ${isLoading}, isAuthenticated: ${isAuthenticated}`);
+
+  if (isLoading) {
+    return (<Label>Loading ....</Label>);
+  }
+
+  if (!isAuthenticated) {
+    return (<Label>not authenticated ....</Label>);
+  }
+
+
+  const view =
+      <FluentProvider theme={webLightTheme}>
+        <div className="App">
+          <MainView />
+        </div>
+      </FluentProvider>;
 
   return view;
 }
-
-export default App;
-
--- static-webapp/src/index.tsx
+++ static-webapp/src/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClientFactory } from './api';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { View1 } from './components/view1/View1';
+import { App } from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import env from './env.json';
+
+const { auth0Domain, auth0ClientId } = env;
+
 
 const client = apolloClientFactory("ignored token");
+const redirect_uri = window.location.origin;
 const mainView =
+  <Auth0Provider domain={auth0Domain} clientId={auth0ClientId} authorizationParams={{
+    redirect_uri, 
+  }} >
+
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>;
+    </ApolloProvider>
+  </Auth0Provider>;
 
 const router = createBrowserRouter([
   { path: "/", element: mainView },
@@ -22,7 +33,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
 );
 
 
